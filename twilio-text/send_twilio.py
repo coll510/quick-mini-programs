feaf2864c2451e23b719777c94f46068333a1916
Key Doby <krdoby@yahoo.com>
2019-06-17 19:03:21 +0000

    update

--- twilio-text/send_twilio.py
+++ twilio-text/send_twilio.py
@@ -3,10 +3,10 @@
 from twilio.rest import Client
 import os
 
-# Your Account SID from twilio.com/console
-account_sid =  os.environ.get("TWILIO_SID") #ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-# Your Auth Token from twilio.com/console
-auth_token  = os.environ.get("TWILIO_TOKEN") #"your_auth_token"
+# Account SID from twilio.com/console
+account_sid =  os.environ.get("TWILIO_SID") 
+# Auth Token from twilio.com/console
+auth_token  = os.environ.get("TWILIO_TOKEN") 
 
 # Create an instance/object of the Twilio class Client
 client = Client(account_sid, auth_token)
